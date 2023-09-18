c6614d5c8d0e0e461cc1b6d00467a775146f5b33
Celso <celsorodriguezsoto@gmail.com>
2023-09-18 10:49:32 -0500

    Añadí un swith de día de la semana

---
+++ practica.js
@@ -0,0 +1,11 @@
+let diaDeLaSemana = "Lunes";
+switch (diaDeLaSemana) {
+    case "Lunes":
+        console.log("Hoy se come pizza");
+        break;
+        case "Martes":
+            console.log("Hoy se come arroz de combinación");
+            break;
+    default:
+        console.log("Especifique el día")
+}
