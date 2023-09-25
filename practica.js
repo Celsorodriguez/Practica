248bb9999f888c66aaac4af6d904b756e0f5ae6d
unknown <celsorodiguezsoto@gmail.com>
2023-09-25 15:03:01 -0600

    Añadí el case de día miércoles y jueves

--- practica.js
+++ practica.js
@@ -6,6 +6,11 @@ switch (diaDeLaSemana) {
         case "Martes":
             console.log("Hoy se come arroz de combinación");
             break;
+        case "Miércoles":
+            console.log("Hoy se come sopa de pata de vaca")
+            break;
+        case "Jueves":
+            console.log("Hoy se come Pure de papa con pollo")
     default:
         console.log("Especifique el día")
 }
